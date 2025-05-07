0d906093453461653343bda87f75b1cc86a0501a
Salvador Alcántara <salvadoralcantara626@gmail.com>
2025-05-06 21:27:10 -0600

    Agregando funcionalidad
    se agregaron los metodos necesarios en MainActivity para el funcionamiento.

--- sharedPreference/app/src/main/java/sv/edu/ues/dam_sharedPrefer/MainActivity.java
+++ sharedPreference/app/src/main/java/sv/edu/ues/dam_sharedPrefer/MainActivity.java
@@ -1,6 +1,13 @@
 package sv.edu.ues.dam_sharedPrefer;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +17,63 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btguardar, btmonstrar, btsalir;
+    private EditText etclave, etvalor;
+    private TextView tvsalida;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Inicializar los elementos de la interfaz
+        btguardar = findViewById(R.id.btnGuardar);
+        btmonstrar = findViewById(R.id.btnMostrar);
+        btsalir = findViewById(R.id.btnSalir);
+        etclave = findViewById(R.id.edtClave);
+        etvalor = findViewById(R.id.edtValor);
+        tvsalida = findViewById(R.id.tvSalida);
+
+        // Guardar en SharedPreferences
+        btguardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences configuracion =
+                        getSharedPreferences("preferences", Context.MODE_PRIVATE);
+                String clave = etclave.getText().toString();
+                String valor = etvalor.getText().toString();
+                SharedPreferences.Editor editorpref = configuracion.edit();
+                editorpref.putString(clave, valor);
+                editorpref.apply();
+                Toast.makeText(MainActivity.this, "Preferencias guardadas con éxito", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Mostrar valor guardado
+        btmonstrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences preferencias =
+                        getSharedPreferences("preferences", Context.MODE_PRIVATE);
+                String clave = etclave.getText().toString();
+                String valor = preferencias.getString(clave, "No existen las preferencias");
+                tvsalida.setText("Clave: " + clave + "\nValor: " + valor);
+            }
+        });
+
+        // Salir de la aplicación
+        btsalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
         });
     }
 }
